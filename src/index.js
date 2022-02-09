29e9bd726a60bc1ff0d4e35b11f4e56caeba2778
Hleb <60222037+GlebasusBP@users.noreply.github.com>
2022-02-10 01:33:10 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  const str = n.toString();
+  const arr = str.split('');
+  const arrRevered = arr.reverse();
+  const arrToString = arrRevered.join('');
+  const reversed = parseFloat(arrToString);
+  if (n[0] % 2 === 0) {
+    return reversed * (-1);
+  }
+  return reversed;
 }
