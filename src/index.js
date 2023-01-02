e91975cf1866fe4d12f01d318f8b0157494a22b9
GlebasusBP <bgp13art@Gmail.com>
2023-01-02 23:17:41 +0300

    refactor: ref function

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 module.exports = function reverse (n) {
-  const str = n.toString();
-  const arr = str.split('');
-  const arrRevered = arr.reverse();
-  const arrToString = arrRevered.join('');
+  const arrToString = n.toString().split('').reverse().join('');
   const reversed = parseFloat(arrToString);
   if (n[0] % 2 === 0) {
     return reversed * (-1);
